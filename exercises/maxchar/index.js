0590b845ca9de02002566cf7038c0f6d9cf088ec
shreyasadiyodi <adiyodishreyas@gmail.com>
2019-03-30 22:39:44 +0530

    maxchar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,19 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    var charCountMap = {};
+    // const arr = str.split('');
+    var maxChar = '';
+    var maxCount = 0;
+    for(let char of str) {
+        charCountMap[char] = charCountMap[char] + 1 || 1;
+        if( charCountMap[char] > maxCount ) {
+            maxChar = char;
+            maxCount = charCountMap[char];
+        }
+    }
+    return maxChar;
+}
 
 module.exports = maxChar;
